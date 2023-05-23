c95e5c4fbe0364da5c9f1581861530df166a0859
midoriba <48616869+midoriba@users.noreply.github.com>
2023-05-23 12:47:54 +0900

    CORSの設定を追加

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,14 @@
 from flask import Flask
 import openai
 from flask import request
+from flask_cors import CORS
 
 app = Flask(__name__)
-@app.route('/sendchat', methods=['POST'])
+CORS(app, resources={r'/api/*':{'origins':['http://127.0.0.1:3000']}})
+@app.route('/api/sendchat', methods=['POST'])
 def send_chat():
-    text = request.form.get('text')
+    text = request.json.get('text')
+    print(f'send: "{text}"', request.json)
     msg = [
         {"role": "user", "content": text}
     ]
@@ -14,6 +17,7 @@ def send_chat():
         messages=msg
     )
     result = res["choices"][0]["message"]["content"]
+    print(f'respond: "{result}"')
     return {"text":result}
 
 if(__name__ == '__main__'):
