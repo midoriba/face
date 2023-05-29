f5875fa1d266d291f65b198fd6c6f6de5105cbd7
midorikawa <48616869+midoriba@users.noreply.github.com>
2023-05-30 00:06:14 +0900

    変数名変更

--- frontend/src/Components/Chat/Message.js
+++ frontend/src/Components/Chat/Message.js
@@ -1,11 +1,11 @@
 import React from "react"
 
-const Message = ({author, text, datetime, left}) => {
+const Message = ({author, content, datetime, left}) => {
     return (
         <div className={"message"+(left ? " left-message" : " right-message")}>
             <div>
                 <p>{author}</p>
-                <p>{text}</p>
+                <p>{content}</p>
                 <p>{datetime}</p>
             </div>
         </div>
