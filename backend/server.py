8667368582cbc7564097685fab3b87c0effb2a49
midoriba <48616869+midoriba@users.noreply.github.com>
2023-07-12 15:47:56 +0900

    音声合成追加

--- backend/server.py
+++ backend/server.py
@@ -4,6 +4,9 @@ from flask import request
 from flask_cors import CORS
 import re
 import json
+import requests as rq
+import json
+import base64
 
 app = Flask(__name__)
 CORS(app, resources={r'/api/*':{'origins':['http://127.0.0.1:3000', 'http://localhost:3000']}})
@@ -16,7 +19,7 @@ def send_chat():
     print(f'send chat: ', request.json)
     res = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[{'role':i['author'], 'content':i['content']} for i in request.json]
+        messages=[{'role':'system', 'content':'あなたは私の友達です。私の話に相槌をうってください。'}]+[{'role':i['author'], 'content':i['content']} for i in request.json]
     )
     chat_response = res["choices"][0]["message"]["content"]
     
@@ -27,14 +30,30 @@ def send_chat():
     res = openai.Completion.create(
         model="text-davinci-003",
         prompt=emotion_prompt,
-        max_tokens=140,
+        max_tokens=70,
         temperature=0.7
     )
     emotion_response = res["choices"][0]["text"]
     print(f'respond emotion: {emotion_response}')
     emotion = json.loads(re.search('{.+}', emotion_response).group())
     print(f'emotion json: {emotion}')
-    return {"content":chat_response, "emotion":emotion}
+    with open('audio_setting.txt', encoding='utf-8', mode='r') as f:
+        audio_id = int(f.read().strip())
+    # 42
+    audio = speak(chat_response, audio_id)
+    return {"content":chat_response, "emotion":emotion, "audio":audio}
 
+def speak(text, speaker=1):
+    params = {'text':text, 'speaker':speaker}
+    response_query = rq.post('http://127.0.0.1:50021/audio_query', params=params)
+    headers = {'Content-Type': 'application/json',}
+    response_audio = rq.post(
+        f'http://127.0.0.1:50021/synthesis',
+        headers=headers,
+        params=params,
+        data=json.dumps(response_query.json())
+    )
+    audio_b64 = base64.b64encode(response_audio.content).decode()
+    return audio_b64
 if(__name__ == '__main__'):
     app.run()
